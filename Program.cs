2011c5f4ddfd13e192ba6d08d80317f513f50246
Holly Knox <hollyknox15@gmail.com>
2025-04-15 10:44:01 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Welcome to the calculator.");
+Console.WriteLine("==========================");
+Console.Write("Please enter a number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Please enter another number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+int result = firstNumber * secondNumber;
+Console.WriteLine("The result is: " + result);
