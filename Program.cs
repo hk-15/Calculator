c5731b38f6222bff288466b7e1473822974bae24
Holly Knox <hollyknox15@gmail.com>
2025-04-15 12:16:07 +0100

    Added operator requirement

--- Program.cs
+++ Program.cs
@@ -2,9 +2,32 @@
 
 Console.WriteLine("Welcome to the calculator.");
 Console.WriteLine("==========================");
+Console.Write("Please enter an operator: ");
+string op = Console.ReadLine();
 Console.Write("Please enter a number: ");
 int firstNumber = int.Parse(Console.ReadLine());
 Console.Write("Please enter another number: ");
 int secondNumber = int.Parse(Console.ReadLine());
-int result = firstNumber * secondNumber;
-Console.WriteLine("The result is: " + result);
+int result;
+switch (op)
+{
+    case "+":
+        result = firstNumber + secondNumber;
+        Console.WriteLine("The result is: " + result);
+        break;
+    case "-":
+        result = firstNumber - secondNumber;
+        Console.WriteLine("The result is: " + result);
+        break;
+    case "*":
+        result = firstNumber * secondNumber;
+        Console.WriteLine("The result is: " + result);
+        break;
+    case "/":
+        result = firstNumber / secondNumber;
+        Console.WriteLine("The result is: " + result);
+        break;
+    default:
+        Console.WriteLine("Invalid operator");
+        break;
+}
